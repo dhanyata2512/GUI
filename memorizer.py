8fcf95145b0f6ceaa7f32b02df101b00f6c9d550
D.P <dhanyata2512@gmail.com>
2024-12-03 18:57:36 +0000

    Update memorizer.py

--- memorizer.py
+++ memorizer.py
@@ -5,14 +5,23 @@ root=Tk()
 root.geometry("500x230")
 root.config(background="#ff8da1")
 
-def code():
+def open_file():
     f=askopenfile(mode="r")
     if f is not None:
-        p=f.read()
+        p=f.readlines()
         print(p)
+        l_box.delete(0,END)
+        for i in p:
+            l_box.insert(END,i)
 
 def s_file():
-    asksaveasfile(defaultextension="*.txt")
+    file=asksaveasfile(defaultextension="*.txt")
+    if file is not None:
+        for item in l_box.get(0,END):
+            print(item,file=file)
+        l_box.delete(0,END)    
+
+
 
 def adding():
     i=input.get()
@@ -21,6 +30,7 @@ def adding():
 
 def remove():
     recive=l_box.curselection()
+    recive=recive[::-1]
     for i in recive:  
         l_box.delete(i)
 
@@ -39,7 +49,7 @@ add.grid(row=2,column=0)
 bottom_frame=Frame(root,background="#ff8da1")
 bottom_frame.pack(pady=10)
 
-open=Button(bottom_frame,text="OPEN",command=code,width=8)
+open=Button(bottom_frame,text="OPEN",command=open_file,width=8)
 open.grid(row=0,column=0,padx=30)
 
 delete=Button(bottom_frame,text="DELETE",width=8,command=remove)
