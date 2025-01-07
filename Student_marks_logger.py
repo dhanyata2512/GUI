f91e424eb12e231091162bb5a55481fa0f17d6e4
D.P <dhanyata2512@gmail.com>
2025-01-07 19:03:10 +0000

    Update Student_marks_logger.py

--- Student_marks_logger.py
+++ Student_marks_logger.py
@@ -6,29 +6,36 @@ root=Tk()
 root.geometry("600x400")
 root.config(background="#FF82AB")
 
-address_book={}
+student_marks={}
 
-def open_file():
-    f=askopenfile(mode="r")
-    if f is not None:
-        p=f.readlines()
-        print(p)
-        l_box.delete(0,END)
-        for i in p:
-            l_box.insert(END,i)
+def adding():
+    n=n_entry.get()
+    if n == "":
+        messagebox.showerror("Error","Name is mandatory!")
+    else:
+        if n not in student_marks:
+            student_marks[n]=(rn_entry.get(),mm_entry.get(),sm_entry.get(),p_entry.get())
+            l_box.insert(END,n)
+            clear_all()
+        else:
+            messagebox.showerror("Error","This name already exists!")
 
-def s_file():
-    file=asksaveasfile(defaultextension="*.txt")
-    if file is not None:
-        for item in l_box.get(0,END):
-            print(item,file=file)
-        l_box.delete(0,END)    
+def clear_all():
+    n_entry.delete(0,END)
+    rn_entry.delete(0,END)
+    mm_entry.delete(0,END)
+    sm_entry.delete(0,END)
+    p_entry.delete(0,END)
 
-def remove():
-    recive=l_box.curselection()
-    recive=recive[::-1]
-    for i in recive:  
-        l_box.delete(i)
+def display(event):
+    top_root=Toplevel(root)
+    index_value=l_box.curselection()
+    if index_value:
+        name=l_box.get(index_value)
+        details=student_marks[name]
+        res1="Name:  "+name+ "\n" +"Roll Number:  "+details[0]+"\n"+"Maths Marks:  "+details[1]+"\n"+"Science Marks:  "+details[2]+"\n"+"Percentage:  "+"\n"+details[3]   
+        final_result=Label(top_root,text=res1)
+        final_result.pack()
 
 top_frame=Frame(root,background="#FF82AB")
 top_frame.pack() 
@@ -60,20 +67,20 @@ right_frame.pack(side=LEFT,pady=20)
 s_mark=Label(right_frame,text="Science marks:")
 s_mark.grid(row=0,column=0,padx=20)
 
-s_entry=Entry(right_frame)
-s_entry.grid(row=0,column=1)
+sm_entry=Entry(right_frame)
+sm_entry.grid(row=0,column=1)
 
 m_mark=Label(right_frame,text="Maths marks:")
 m_mark.grid(row=1,column=0,padx=20,pady=20)
 
-m_entry=Entry(right_frame)
-m_entry.grid(row=1,column=1)
+mm_entry=Entry(right_frame)
+mm_entry.grid(row=1,column=1)
 
 percentage=Label(right_frame,text="Percentage:")
 percentage.grid(row=2,column=0,padx=20)
 
-percentage_entry=Entry(right_frame)
-percentage_entry.grid(row=2,column=1)
+p_entry=Entry(right_frame)
+p_entry.grid(row=2,column=1)
 
 bm_frame=Frame(root,background="#FF82AB")
 bm_frame.pack()
@@ -83,6 +90,7 @@ s_bar.grid(row=0,column=1)
 
 l_box=Listbox(bm_frame,yscrollcommand=s_bar.set,width=70)
 l_box.grid(row=0,column=0)
+l_box.bind("<<ListboxSelect>>",display)
 
 bottom_frame=Frame(root,background="#FF82AB")
 bottom_frame.pack(pady=20)
@@ -90,16 +98,16 @@ bottom_frame.pack(pady=20)
 edit=Button(bottom_frame,text="Edit",width=8)
 edit.grid(row=1,column=0)
 
-delete=Button(bottom_frame,text="DELETE",width=8,command=remove)
+delete=Button(bottom_frame,text="DELETE",width=8)
 delete.grid(row=1,column=1,padx=20)
 
-open=Button(bottom_frame,text="OPEN",command=open_file,width=8)
+open=Button(bottom_frame,text="OPEN",width=8)
 open.grid(row=1,column=2)
 
-add=Button(bottom_frame,text="UPDATE/ADD")
+add=Button(bottom_frame,text="UPDATE/ADD",command=adding)
 add.grid(row=1,column=3,padx=20)
 
-save=Button(bottom_frame,text= "SAVE",command=s_file,width=20)
+save=Button(bottom_frame,text= "SAVE",width=20)
 save.grid(row=1,column=4)
 
 s_bar.config(command=l_box.yview) 
